4ab82fba239d11e4c20d91f7155e4f6d09de7aec
kareenhermintia <kareenhermintia19@gmail.com>
2024-12-20 10:43:49 +0700

    second commit add table coloumn index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latian Git</title>
+</head>
+<body>
+    <h1>Latihan git</h1>
+    <tr>
+        <td>Nama</td>
+        <td>Kelas</td>
+    </tr>
+    <tr>
+        <td>Kareen</td>
+        <td>Kelas XML</td>
+    </tr>
+    <button>Submit</button>
+</body>
+</html>
